ee603754c46675ddfcc2eb7c3e8d78ea63489d11
Noor <nr5dr@virginia.edu>
2020-10-09 15:49:24 -0400

    Updates

---
+++ precache-manifest.40fc10a74c281c8e386599f0b07140d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a721796dcbdfbed8f435b2cf889af4",
+    "url": "/todolist-basic/index.html"
+  },
+  {
+    "revision": "024547b21ddfdf20b2fc",
+    "url": "/todolist-basic/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "caca910c1997d471c4bc",
+    "url": "/todolist-basic/static/css/main.21a1484d.chunk.css"
+  },
+  {
+    "revision": "024547b21ddfdf20b2fc",
+    "url": "/todolist-basic/static/js/2.ecee3597.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/todolist-basic/static/js/2.ecee3597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caca910c1997d471c4bc",
+    "url": "/todolist-basic/static/js/main.155a98a5.chunk.js"
+  },
+  {
+    "revision": "3fb5d1923fb359e46962",
+    "url": "/todolist-basic/static/js/runtime-main.4d32bb56.js"
+  }
+]);
